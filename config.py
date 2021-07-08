adb46341bba5fba255a48bce82a16b4ffa6c1141
pcLLY <86560633+pcLLY@users.noreply.github.com>
2021-07-08 11:25:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+	return '''Follow for follow?'''.format(name)
 
 scheduler_time = 15 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "BadFootballTakz" #change this to yours
